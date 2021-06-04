26f2818dce412eafbe6871a14240e7814d2d1c86
Deepak <deepak.19jics057@jietjodhpur.ac.in>
2021-06-04 10:41:04 +0530

    closure

---
+++ Day10/script/index.js
@@ -0,0 +1,35 @@
+//print 
+// function tester(){
+//     for(var i=1;i<=5;i++){
+//         setTimeout(function (){
+//             console.log(i);
+//         }, i*1000);
+//     }
+// }
+// tester()
+
+//print 
+// function tester(){
+//     for(var i=1;i<=5;i++){
+//         setTimeout(function (){
+//             console.log(i++);
+//         }, i*1000);
+//     }
+// }
+// tester()
+
+
+// print 1 to 5
+function timer(){
+    for(var i=1;i<=5;i++){
+        setTimeout(function (){
+            console.log(i++ - 5);
+        }, i*1000);
+    }
+}
+timer()
+
+
+
+
+
---
+++ Day9/script/index.js
@@ -0,0 +1,17 @@
+// max ans min of a array
+array = [58, 52, 64, 75, 65, 98, 48, 20, 2, 10, 1, 5];
+console.log(array);
+console.log(Math.min(...array));
+console.log(Math.max(...array));
+
+// Reverse a number
+let array= [10,9,8,7,6,5,4,3,2,1];
+function swap(){
+    let a,b,c;
+    c=a;
+    a=b;
+    b=c;
+}
+for (let i = 0; index < array.length; index++) {
+    
+}

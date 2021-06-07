c3058224799f93cd1ec7a25ceda4f2fecfbec541
Deepak <deepak.19jics057@jietjodhpur.ac.in>
2021-06-07 16:31:07 +0530

    Assignment

--- Day10/script/index.js
+++ Day10/script/index.js
@@ -1,12 +1,12 @@
 //print 
-// function tester(){
-//     for(var i=1;i<=5;i++){
-//         setTimeout(function (){
-//             console.log(i);
-//         }, i*1000);
-//     }
-// }
-// tester()
+//  function tester(){
+//      for(var i=1;i<=5;i++){
+//          setTimeout(function (){
+//              console.log(i);
+//          }, i*1000);
+//      }
+//  }
+//  tester()
 
 //print 
 // function tester(){
@@ -20,14 +20,15 @@
 
 
 // print 1 to 5
-function timer(){
+ function timer(){
     for(var i=1;i<=5;i++){
-        setTimeout(function (){
-            console.log(i++ - 5);
+       setTimeout(function (){
+             console.log(i++ - 5);
         }, i*1000);
-    }
-}
-timer()
+     }
+ }
+ timer()
+
 
 
 

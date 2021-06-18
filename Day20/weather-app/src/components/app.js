077a28d2d365399b4aab79acbc05d916f3a8d671
Deepak <deepak.19jics057@jietjodhpur.ac.in>
2021-06-18 08:26:26 +0530

    react-projects

---
+++ Day20/weather-app/src/components/app.js
@@ -0,0 +1,15 @@
+// import React, { component } from 'react';
+// import  ReactDOM  from 'react-dom';
+
+
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         postion => console.log(postion),
+//         err => console.log(err)
+//     )
+//     return(
+//      <h1>Hello Everyone</h1>
+//     );
+// }
+
+// export default App;
---
+++ Day21/clock-project/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import clock from './components/clock';
+
+
+ReactDOM.render(
+   <clock />,
+  document.getElementById('root')
+);
+
+
